2711e45c9b4569bee2c0a0e6d3b2a0574e1331e1
rickyduran <2920811635@qq.com>
2025-05-04 22:47:18 +0800

    Lambda_Expressions has been modified

--- Lambda_Expressions.java
+++ Lambda_Expressions.java
@@ -1,15 +1,32 @@
 public class Lambda_Expressions {
   public static void main(String[] args) {
-    sendMessage(() -> System.out.println("Testing Lambda Expressions!"));
+    // 1 创建类继承函数式接口并调用sendMessage方法
+    send s = new ZhouHao();
+    s.sendMessage("Zhou Hao");
+    // 2 使用匿名内部类
+    sendMessage(new send() {
+      @Override
+      public void sendMessage(String s) {
+       System.out.println(s); 
+      }
+    });
+    // 3 使用Lambda表达式，简化了匿名内部类的写法
+    sendMessage((message) -> System.out.println(message));
   }
 
   public static void sendMessage(send s)
   {
-    s.sendMessage();
+    s.sendMessage("Testing Lambad operations!");
   }
 }
 
 interface send{
-  public void sendMessage();
+  public void sendMessage(String s);
 }
 
+class ZhouHao implements send{
+  @Override
+  public void sendMessage(String s) {
+    System.out.println("This is from " + s + "!");
+  }
+}
