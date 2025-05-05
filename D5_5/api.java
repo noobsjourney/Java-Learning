0a73c699a1bab15a2e91879c1e1f7a44c52a7984
rickyduran <2920811635@qq.com>
2025-05-05 11:19:05 +0800

    update already

---
+++ D5_5/api.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class api {
+  public static void main(String[] args) {
+    // 1 Arrays toString方法 将数组转换为一个字符串输出
+    String[] name = {"ricky","duran","bonhiver"};
+    System.out.println(Arrays.toString(name));
+    // 2 Arrays asList方法 传入一个数组，返回一个List集合
+    List<String> list = Arrays.asList("a","b","c");
+    System.out.println(list);
+
+  }
+}
