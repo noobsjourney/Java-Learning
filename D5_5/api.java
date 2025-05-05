8bb495685e54128358662f1e697dd50ac438da91
rickyduran <2920811635@qq.com>
2025-05-05 11:46:35 +0800

    api.java modified

--- D5_5/api.java
+++ D5_5/api.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class api {
@@ -9,6 +10,7 @@ public class api {
     // 2 Arrays asList方法 传入一个数组，返回一个List集合
     List<String> list = Arrays.asList("a","b","c");
     System.out.println(list);
-
+    // 3
+    Collections.sort(list);
   }
 }
